c773cd451543a37852c90a01329563b67ae566b1
Michael Bruce Allen <CodeAmend@gmail.com>
2025-06-28 22:07:25 -0400

    Refactor test for mock note validation

--- lambdas/note-create/index.test.js
+++ lambdas/note-create/index.test.js
@@ -29,59 +29,57 @@ describe('note-create Lambda', () => {
   // });
 
   describe("Validation", () => {
+
     beforeEach(() =>{
       mockNote = { ...mockNoteSightreading }
     })
 
-    it('returns 400 if rep_id is missing', async () => {
-      delete mockNote.rep_id;
-      const res = await handler({ body: mockNote });
+    async function expectFunc(mock) {
+      const res = await handler({ body: mock });
       expect(res).toStrictEqual({
         statusCode: 400,
         body: JSON.stringify({ error: MISSING_SCHEMA_VALUES })
       });
+    }
+
+    it('returns 400 if rep_id is missing', async () => {
+      delete mockNote.rep_id;
+      expectFunc(mockNote);
     })
 
     it('returns 400 if note_id is missing', async () => {
       delete mockNote.note_id;
-      const res = await handler({ body: mockNote });
-      expect(res.statusCode).toBe(400);
+      expectFunc(mockNote);
     })
 
     it('returns 400 if time is missing', async () => {
       delete mockNote.time;
-      const res = await handler({ body: mockNote });
-      expect(res.statusCode).toBe(400);
+      expectFunc(mockNote);
     })
 
     it('returns 400 if time is missing', async () => {
       delete mockNote.date;
-      const res = await handler({ body: mockNote });
-      expect(res.statusCode).toBe(400);
+      expectFunc(mockNote);
     })
 
     it('returns 400 if duration_minutes is missing', async () => {
       delete mockNote.duration_minutes;
-      const res = await handler({ body: mockNote });
-      expect(res.statusCode).toBe(400);
+      expectFunc(mockNote);
     })
 
     it('returns 400 if entries is missing', async () => {
       delete mockNote.entries;
-      const res = await handler({ body: mockNote });
-      expect(res.statusCode).toBe(400);
+      expectFunc(mockNote);
     })
 
     it('returns 400 if entries does not have', async () => {
       delete mockNote.entries;
-      const res = await handler({ body: mockNote });
-      expect(res.statusCode).toBe(400);
+      expectFunc(mockNote);
     })
 
     it('returns 400 if entries does not have', async () => {
       mockNote.entries = [];
-      const res = await handler({ body: mockNote });
-      expect(res.statusCode).toBe(400);
+      expectFunc(mockNote);
     })
   })
 });
